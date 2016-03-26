84b92b8dc97b9bab5350527ad4d834e571fa2052
Anthony Kim <ak3703@columbia.edu>
2016-03-26 18:02:59 -0400

    commented code

--- ticketer.py
+++ ticketer.py
@@ -2,27 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from threading import Thread
-import Queue
-import sys
+import Queue, sys
+
 q = Queue.Queue()
 
 def get_tickets():
-    
+    ''' opens a new chrome application and clicks on the register then checkout button'''
+    # MANDATORY: include direct path to chromedriver here
     driver = webdriver.Chrome('/Users/AnthonyKim/Documents/workspace/tdwytb/chromedriver')
-    driver.get("https://www.eventbrite.com/e/bacchanal-2016-tickets-22507236751")
-
+    # opens bacchanal eventbrite ticketing page
+    driver.get("https://www.eventbrite.com/e/practice-tickets-24208427057")
+    # click register button
     register = driver.find_element(By.XPATH, '//*[@id="event-page"]/main/div[1]/div[2]/div/div[1]/div[1]/div/div[2]/div/div/div/form/span/span[2]/a').click()
-
+    # click checkout buton
     checkout = driver.find_element(By.XPATH, '//*[@id="event-page"]/div[7]/div[2]/div/div/section[3]/div/div/div[3]/input').click()
 
 
 if __name__ == "__main__":
-
-         
+    # attempt to get 5 tickets
     for i in range(5):
+        # new thread for each ticket grabber
         thread = Thread(target=get_tickets, args = '')
         thread.start()
-        q.put(thread)
+        q.put(thread) # add the thread to the queue
     try:
         while 1:
             x = 1
