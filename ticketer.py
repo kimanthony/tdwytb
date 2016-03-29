016a9313196c7d054e1ff1152274ebf58c1d1316
Anthony Kim <ak3703@columbia.edu>
2016-03-28 23:49:00 -0400

    ticketer.py will keep refreshing pages until it hits the registration page

--- ticketer.py
+++ ticketer.py
@@ -13,19 +13,22 @@ def get_tickets():
     # MANDATORY: include direct path to chromedriver here
     driver = webdriver.Chrome(os.path.dirname(os.path.realpath(__file__)) + '/chromedriver')
     # wait until time is equal or after ticket_time to open the site and click the buttons
-    while datetime.now() < ticket_time:
-        pass
-    # opens bacchanal eventbrite ticketing page
-    driver.get("https://www.eventbrite.com/e/bacchanal-2016-tickets-22507236751")
-    # click register button
-    register = driver.find_element(By.XPATH, '//*[@id="event-page"]/main/div[1]/div[2]/div/div[1]/div[1]/div/div[2]/div/div/div/form/span/span[2]/a').click()
-    # click checkout buton
-    checkout = driver.find_element(By.XPATH, '//*[@id="event-page"]/div[7]/div[2]/div/div/section[3]/div/div/div[3]/input').click()
+    while 1:
+        try:
+            # opens bacchanal eventbrite ticketing page
+            driver.get("https://www.eventbrite.com/e/bacchanal-2016-tickets-22507236751")
+            # click register button
+            register = driver.find_element(By.XPATH, '//*[@id="event-page"]/main/div[1]/div[2]/div/div[1]/div[1]/div/div[2]/div/div/div/form/span/span[2]/a').click()
+            # click checkout buton
+            checkout = driver.find_element(By.XPATH, '//*[@id="event-page"]/div[7]/div[2]/div/div/section[3]/div/div/div[3]/input').click()
+        except:
+            print("continuing")
+            continue
 
 
 if __name__ == "__main__":
     # attempt to get 5 tickets
-    for i in range(5):
+    for i in range(7):
         # new thread for each ticket grabber
         thread = Thread(target=get_tickets, args = '')
         thread.start()
